3eb18a56d14a311918ef126dbcb22f388ea7d897
Sujeeth Jinesh <admin@admins-MacBook-Pro-3.local>
2016-09-24 11:48:38 -0400

    Updated item screen page and json parsing

--- android-client/QuickCart/app/src/main/java/com/example/admin/quickcart/Item.java
+++ android-client/QuickCart/app/src/main/java/com/example/admin/quickcart/Item.java
@@ -6,8 +6,44 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import java.io.Serializable;
+
 /**
  * Created by admin on 9/24/16.
  */
-public class Item {
+public class Item implements Serializable {
+
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private int quantity;
+
+    public Item(int id, String name, String description, double price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
 }
--- android-client/QuickCart/app/src/main/java/com/example/admin/quickcart/ItemScreen.java
+++ android-client/QuickCart/app/src/main/java/com/example/admin/quickcart/ItemScreen.java
@@ -1,15 +1,39 @@
 package com.example.admin.quickcart;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by admin on 9/24/16.
  */
 public class ItemScreen extends Activity {
+
+    TextView descriptionView;
+    TextView quantityView;
+    TextView priceView;
+    TextView nameView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item_screen);
+
+        Intent intent = getIntent();
+        ArrayList<Item> items = (ArrayList<Item>) intent.getSerializableExtra("items");
+        int position = (int) intent.getSerializableExtra("position");
+
+        descriptionView = (TextView) findViewById(R.id.description);
+        quantityView = (TextView) findViewById(R.id.quantity);
+        priceView = (TextView) findViewById(R.id.price);
+        nameView = (TextView) findViewById(R.id.name);
+
+        descriptionView.setText(items.get(position).getDescription());
+        quantityView.setText(items.get(position).getQuantity() + "");
+        priceView.setText(Double.toString(items.get(position).getPrice()));
+        nameView.setText(items.get(position).getName());
     }
 }
